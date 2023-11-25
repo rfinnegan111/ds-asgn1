3353fe9c1c692f7c8dad7f36fd2d26634463f063
Rebecca Finnegan <finneganr111@gmail.com>
2023-11-25 15:10:27 +0000

    Setting Up

---
+++ test/ds-asgn1-main.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DsAsgn1Main from '../lib/ds-asgn1-main-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ds-asgn1-main-stack.ts
+test('SQS Queue Created', () => {
+    //   const app = new cdk.App();
+    //     // WHEN
+    //   const stack = new DsAsgn1.DsAsgn1MainStack(app, 'MyTestStack');
+    //     // THEN
+    //   const template = Template.fromStack(stack);
+    
+    //   template.hasResourceProperties('AWS::SQS::Queue', {
+    //     VisibilityTimeout: 300
+    //   });
+    });
